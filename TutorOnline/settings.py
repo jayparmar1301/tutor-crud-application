e056dd493d89f9cf34da03634b37fe4b45c357df
Jay Parmar <parmarjay376@gmail.com>
2022-04-11 23:34:42 +0530

    Database: Added Postgresql database to the project

--- TutorOnline/settings.py
+++ TutorOnline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'TutorOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,13 +76,15 @@ WSGI_APPLICATION = 'TutorOnline.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'tutor_db',
+       'USER': 'postgres',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
--- tutor/models.py
+++ tutor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# This is model related to tutor information
+class Tutor_Info(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    tutor_skills = models.CharField(max_length=250, null=True)
+    tutor_exp = models.IntegerField(blank=True, null=True)
+
