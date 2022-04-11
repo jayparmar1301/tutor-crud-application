9400ef26cdeb2a191519660fcc90f2b44f12eb53
Jay Parmar <parmarjay376@gmail.com>
2022-04-11 23:37:57 +0530

    Pages: 'Homepage' and 'Add tutor' to the project

--- tutor/urls.py
+++ tutor/urls.py
@@ -3,7 +3,9 @@ from . import  views
 
 urlpatterns = [
     path('', views.homepage),
-    path('edit', views.edit_tutor),
+    path('add', views.add_tutor_page),
+    path('add/tutor', views.add_tutor),
+    path('add/tutor/id={id}', views.edit_tutor),
     path('delete', views.delete_tutor),
     path('update', views.update_tutor)
 ]
--- tutor/views.py
+++ tutor/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from matplotlib.style import context
+from .models import Tutor_Info
 
 def homepage(request):
-    return HttpResponse('Homepage')
+    tutor_info = Tutor_Info.objects.all()
+    return render(request, 'homepage.html', context = {'tutors': tutor_info})
+
+def add_tutor(request):
+    first_name = request.GET.get('first_name')
+    last_name = request.GET.get('last_name')
+    email = request.GET.get('email')
+    tutor_skills = request.GET.get('tutor_skills')
+    tutor_exp = request.GET.get('tutor_exp')
+    Tutor_Info.objects.create(first_name=first_name,
+                            last_name = last_name,
+                            email = email,
+                            tutor_skills = tutor_skills,
+                            tutor_exp = tutor_exp)
+
+    return redirect('/tutor')
+
+def add_tutor_page(request):
+    return render(request, 'add_tutor.html')
 
 def edit_tutor(request):
     return HttpResponse('Edit')
