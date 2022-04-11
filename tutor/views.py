8e5703c8944e372717e65c7ebe0fe6a65a3add06
Jay Parmar <parmarjay376@gmail.com>
2022-04-12 00:43:20 +0530

    Validation: made email field disable in edit

--- tutor/views.py
+++ tutor/views.py
@@ -38,7 +38,6 @@ def update_tutor(request, id):
     tutor = Tutor_Info.objects.get(id=id)
     tutor.first_name = request.GET.get('first_name') if request.GET.get('first_name')!= None else tutor.first_name
     tutor.last_name = request.GET.get('last_name') if request.GET.get('last_name')!= None else tutor.last_name
-    tutor.email = request.GET.get('email') if request.GET.get('email')!= None else tutor.email
     tutor.tutor_skills = request.GET.get('tutor_skills') if request.GET.get('tutor_skills') else tutor.tutor_skills
     tutor.tutor_exp = request.GET.get('tutor_exp') if request.GET.get('tutor_exp') else tutor.tutor.exp
     tutor.save()
