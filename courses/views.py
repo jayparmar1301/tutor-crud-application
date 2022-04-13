411100e0e1ab2610a19998e4a3d2bdd229c4c4b7
Jay Parmar <parmarjay376@gmail.com>
2022-04-13 14:30:17 +0530

    Expection Handling: Added error handling in application

--- courses/views.py
+++ courses/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import redirect, render
 from matplotlib.style import context
 from .models import Course_Info
 from tutor.models import Tutor_Info
+from django.core.exceptions import  ObjectDoesNotExist
 
 def tutor_courses_homepage(request, id):
-    tutor = Tutor_Info.objects.get(id=id)
+    try:
+        tutor = Tutor_Info.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No Tutor info found' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     courses = Course_Info.objects.filter(tutor_id=tutor.id)
     return render(request, 'tutor_courses.html', context = {'courses': courses, 'tutor': tutor})
 
@@ -19,27 +26,48 @@ def add_new_course(request):
     course_name = request.GET.get('course_name')
     course_duration = request.GET.get('course_duration')
     course_description = request.GET.get('course_description')
-    Course_Info.objects.create(course_name = course_name,
-                                course_duration = course_duration,
-                                course_description = course_description,
-                                tutor_id = tutor)
+    try:
+        Course_Info.objects.create(course_name = course_name,
+                                    course_duration = course_duration,
+                                    course_description = course_description,
+                                    tutor_id = tutor)
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
 
     return redirect(f'/courses/{tutor_id}')
 
 def edit_course(request, id):
-    course = Course_Info.objects.get(id=id)
+    try:
+        course = Course_Info.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No course found with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     return render(request, 'edit_course_info.html', context = { 'course': course })
 
 def update_course(request, id):
-    course = Course_Info.objects.get(id=id)
-    course.course_name = request.GET.get('course_name') if request.GET.get('course_name') != None else course.course_name
-    course.course_duration = request.GET.get('course_duration') if request.GET.get('course_duration') else course.course_duration
-    course.course_description = request.GET.get('course_description') if request.GET.get('course_description') else course.course_description
-    course.save()
+    try:
+        course = Course_Info.objects.get(id=id)
+        course.course_name = request.GET.get('course_name') if request.GET.get('course_name') != None else course.course_name
+        course.course_duration = request.GET.get('course_duration') if request.GET.get('course_duration') else course.course_duration
+        course.course_description = request.GET.get('course_description') if request.GET.get('course_description') else course.course_description
+        course.save()
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No course found with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     return redirect(f'/courses/{course.tutor_id.id}')
 
 def delete_course(request, id):
-    course = Course_Info.objects.get(id=id)
+    try:
+        course = Course_Info.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No course found with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     tutor_id = course.tutor_id.id
     course.delete()
     return redirect(f'/courses/{tutor_id}')
--- tutor/views.py
+++ tutor/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from matplotlib.style import context
 from .models import Tutor_Info
+from django.core.exceptions import ObjectDoesNotExist
 
 def homepage(request):
     tutor_info = Tutor_Info.objects.all()
@@ -13,11 +14,14 @@ def add_tutor(request):
     email = request.GET.get('email')
     tutor_skills = request.GET.get('tutor_skills')
     tutor_exp = request.GET.get('tutor_exp')
-    Tutor_Info.objects.create(first_name=first_name,
-                            last_name = last_name,
-                            email = email,
-                            tutor_skills = tutor_skills,
-                            tutor_exp = tutor_exp)
+    try:
+        Tutor_Info.objects.create(first_name=first_name,
+                                last_name = last_name,
+                                email = email,
+                                tutor_skills = tutor_skills,
+                                tutor_exp = tutor_exp)
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
 
     return redirect('/tutor')
 
@@ -25,19 +29,37 @@ def add_tutor_page(request):
     return render(request, 'add_tutor.html')
 
 def edit_tutor(request, id):
-    tutor = Tutor_Info.objects.get(id=id)
+    try:
+        tutor = Tutor_Info.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No tutor with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     return render(request, 'edit_tutor.html', context = {'tutor': tutor})
 
 def delete_tutor(request, id):
-    tutor = Tutor_Info.objects.get(id=id)
+    try:
+        tutor = Tutor_Info.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No tutor with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     tutor.delete()
     return redirect('/tutor')
 
 def update_tutor(request, id):
-    tutor = Tutor_Info.objects.get(id=id)
-    tutor.first_name = request.GET.get('first_name') if request.GET.get('first_name')!= None else tutor.first_name
-    tutor.last_name = request.GET.get('last_name') if request.GET.get('last_name')!= None else tutor.last_name
-    tutor.tutor_skills = request.GET.get('tutor_skills') if request.GET.get('tutor_skills') else tutor.tutor_skills
-    tutor.tutor_exp = request.GET.get('tutor_exp') if request.GET.get('tutor_exp') else tutor.tutor.exp
-    tutor.save()
+    try:
+        tutor = Tutor_Info.objects.get(id=id)
+        tutor.first_name = request.GET.get('first_name') if request.GET.get('first_name')!= None else tutor.first_name
+        tutor.last_name = request.GET.get('last_name') if request.GET.get('last_name')!= None else tutor.last_name
+        tutor.tutor_skills = request.GET.get('tutor_skills') if request.GET.get('tutor_skills') else tutor.tutor_skills
+        tutor.tutor_exp = request.GET.get('tutor_exp') if request.GET.get('tutor_exp') else tutor.tutor.exp
+        tutor.save()
+    except ObjectDoesNotExist:
+        return render(request, 'exception_page.html', context = {'error':  'No tutor with this info' })
+    except Exception as ex:
+        return render(request, 'exception_page.html', context = {'error':  ex })
+
     return redirect('/tutor')
